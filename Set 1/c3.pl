db72eb30324d636265af6e0efc12ec58ad8324b2
Alexander <rogueshy@derpymail.org>
2017-01-16 16:07:35 +0300

    Update c3.pl
    got rid of the unused pieces

--- Set 1/c3.pl
+++ Set 1/c3.pl
@@ -9,24 +9,14 @@ my $h1_le = length ($hex1);
 my $str1 = pack ("H*", $hex1);
 my $s1_le = length ($str1);
 
-my $str3 = "a";
-for ($i = 1; $i < 34; $i++) {
-	$str3 = $str3 . "a";
-}
-print $str3;
-
 foreach my $char (split //, $str2) {
 	print "=== \n";
 	print "Character: $char \n";
 	my $line = $char;
-	for ($i = 1; $i < 34; $i++) {
+	for ($i = 1; $i < $s1_le; $i++) {
 		$line = $line . $char;
 	}
 	my $unhex = $str1 ^ $line;
-	print "unhexed: $unhex \n";
-	my $hex2 = unpack ("H*", $line);
-	print "Hexed character: $hex2 \n";
-	my $hex3 = $hex1 ^ $hex2;
-	print "Line: $hex3 \n";
+	print "Answer: $unhex \n";
 	print "=== \n";
 }
