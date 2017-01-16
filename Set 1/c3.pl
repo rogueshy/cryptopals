bcf265f361cfa0a9746d7b6ad97d0f0c900194b8
Alexander <rogueshy@derpymail.org>
2017-01-16 17:11:54 +0300

    Update for only correct answer is shown.

--- Set 1/c3.pl
+++ Set 1/c3.pl
@@ -1,22 +1,23 @@
 use strict;
 use warnings;
-use MIME::Base64;
-my $str2 = 'thefiveboXingwizardsjumpquickly'; #Pangram
-my $i;
-my $hex1= '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736';
-my $h1_le = length ($hex1);
 
+my $pan = 'thefiveboxingwizardsjumpquicklyTHEFIVEBOXINGWIZARDSJUMPQUICKLY'; #Pangram
+my $hex1= '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'; #input
+my $h1_le = length ($hex1);
 my $str1 = pack ("H*", $hex1);
 my $s1_le = length ($str1);
 
-foreach my $char (split //, $str2) {
-	print "=== \n";
-	print "Character: $char \n";
+
+my $i;
+my $re = qr/\s([tT]he)\s|(ing)|\s(a)\s|\s(an)\s/p;
+foreach my $char (split //, $pan) {
 	my $line = $char;
 	for ($i = 1; $i < $s1_le; $i++) {
 		$line = $line . $char;
 	}
 	my $unhex = $str1 ^ $line;
-	print "Answer: $unhex \n";
-	print "=== \n";
+	if ($unhex =~ /$re/g ) {
+		print "Character: $char \n";
+		print "Answer: $unhex \n"; 
+		}	
 }
